c99e0407933cfae062d48f672115d6821ce56221
Dr. X. Treme <lafe@orgasmatronics.com>
2015-09-02 11:22:09 -0600

    Pulse Data Button
    when button is pressed, a pulse is applied to the coil, data are recorded and sent down the serial line.

---
+++ pulseDataButton.ino
@@ -0,0 +1,45 @@
+/*
+simple program to get time domain response to a pulse
+
+
+Aleister Crowley liscense: do what thou wilt shall be the whole of the law
+/*
+
+int pulseLength = 25;//ms
+boolean buttonState = false;
+int numPoints = 500; //number of points
+int deltaT = 1;
+int x0 = 0;
+
+void setup(){
+    Serial.begin(115200);
+    x0 = analogRead(A5);
+}
+
+void loop(){
+  
+  buttonState = digitalRead(2);
+  if(buttonState){
+    analogWrite(5,255);
+    delay(pulseLength);
+    analogWrite(5,0);
+    delay(1);
+    int index = 0;
+    int x[numPoints];
+    for(index = 0;index < numPoints;index++){
+      x[index] = analogRead(A5)  - x0;
+      delay(deltaT);
+    }
+    for(index = 0;index < numPoints;index++){
+      Serial.print(x[0]);
+      Serial.print("  ");
+    }
+    Serial.println();
+    delay(500);
+    }
+    
+  }
+    
+    
+    
+    
