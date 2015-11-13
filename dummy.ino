8c5b2cfb9ede2fe10fef7d126694addc30716093
Dr. X. Treme <lafe@orgasmatronics.com>
2015-11-12 20:09:24 -0700

    Create dummy.ino

---
+++ dummy.ino
@@ -0,0 +1,26 @@
+/*
+Crowley license: do what thou wilt shall be the whole of the law
+fuck patents, fuck copyright
+
+November 2015
+
+read from D11, turn on transistor if it's high, off if low
+
+*/
+
+
+int dataPin = 11;
+
+
+void setup() {
+  pinMode(dataPin, INPUT);
+}
+
+void loop() {
+
+  int dataState = digitalRead(dataPin);
+  if(dataState != 0){
+    analogWrite(10,255);
+  }
+  
+}
